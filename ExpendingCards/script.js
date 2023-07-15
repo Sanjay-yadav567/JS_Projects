c1ba1c0bfb77efc570b7b01debe943c4b009c718
Sanjay Yadav <72335676+Sanjay-yadav567@users.noreply.github.com>
2023-07-15 11:23:40 +0530

    Add files via upload

---
+++ ExpendingCards/script.js
@@ -0,0 +1,15 @@
+// change the active class ;
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach(panel =>{
+    panel.addEventListener('click',()=>{
+        removeactiveclasses();
+        panel.classList.add('active'); 
+    })
+})
+
+function removeactiveclasses(){
+    panels.forEach(panel=>{
+        panel.classList.remove('active');
+    })
+}
